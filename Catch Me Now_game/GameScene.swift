40e7858cfc2df12abec46cc828e437043b1e721e
Testing_author <testing_beta@mail.com>
2020-02-29 20:03:21 +0200

    Made the backgroundImage ignore any transparencies and be placed behind everything else that might appear on the screen.

--- Catch Me Now_game/GameScene.swift
+++ Catch Me Now_game/GameScene.swift
@@ -13,8 +13,8 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         let backgroundImage = SKSpriteNode(imageNamed: "background")
         backgroundImage.position = CGPoint(x: 512, y: 384)
+        backgroundImage.blendMode = .replace //ignoring any transparencies
+        backgroundImage.zPosition = -1  //makes this backgroundImage be placed behind everything else
         addChild(backgroundImage)
-        
-        
     }
 }
